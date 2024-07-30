52fc0c8e81afdb2682f60fe87b25ee588274dd2f
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-07-30 09:28:21 +0200

    feat: crate first sketch of dm command

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log("Hello, world!");
+import dotenv from "dotenv";
+import {
+	Client,
+	RichPresence,
+	CustomStatus,
+	SpotifyRPC,
+} from "discord.js-selfbot-v13";
+
+dotenv.config();
+
+const client = new Client();
+
+client.on("ready", async () => {
+	console.log(`${client.user?.username} is ready!`);
+});
+
+// If i send !dm to the bot, it will send dm to every user in every guilds that the bot is in
+client.on("messageCreate", async (message) => {
+	if (message.content === "!dm") {
+		client.guilds.cache.forEach((guild) => {
+			guild.members.cache.forEach((member) => {
+				if (member.id !== client.user?.id) {
+					member.send("Hello!");
+				}
+			});
+		});
+	}
+});
+
+client.login(process.env.TOKEN);
