7e9b88e69b057d54d1060e1a6bf852409d811efe
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-08-02 22:57:50 +0200

    chore: update eslint rules and package dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,25 @@
 import dotenv from "dotenv";
-import {
-	Client,
-} from "discord.js-selfbot-v13";
+import { Client } from "discord.js-selfbot-v13";
 
 dotenv.config();
 
 const client = new Client();
 
 client.on("ready", async () => {
-	console.log(`${client.user?.username} is ready!`);
+    console.log(`${client.user?.username} is ready!`);
 });
 
 // If i send !dm to the bot, it will send dm to every user in every guilds that the bot is in
 client.on("messageCreate", async (message) => {
-	if (message.content === "!dm") {
-		client.guilds.cache.forEach((guild) => {
-			guild.members.cache.forEach((member) => {
-				if (member.id !== client.user?.id) {
-					member.send("Hello!");
-				}
-			});
-		});
-	}
+    if (message.content === "!dm") {
+        client.guilds.cache.forEach((guild) => {
+            guild.members.cache.forEach((member) => {
+                if (member.id !== client.user?.id) {
+                    member.send("Hello!");
+                }
+            });
+        });
+    }
 });
 
 client.login(process.env.TOKEN);
