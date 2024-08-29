96ab65d1d74657423b5b0052421817738bcc989c
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2024-08-29 18:16:11 +0200

    chore: changed formatter (prettier)

--- src/helpers/proxyChooser.ts
+++ src/helpers/proxyChooser.ts
@@ -1,7 +1,7 @@
 import path from "path";
 import fs from "fs";
 import { Collection } from "@discordjs/collection";
-import z from "zod"
+import z from "zod";
 
 interface ProxyI {
     ip: string;
@@ -16,26 +16,31 @@ interface ProxyI {
 class Proxy implements ProxyI {
     ip = "";
     port = "";
-    auth?: { username: string; password: string; } | undefined;
+    auth?: { username: string; password: string } | undefined;
 
     constructor(stringProxy: string) {
         this.validateProxy(stringProxy);
     }
 
     private validateProxy(stringProxy: string) {
-        const ipSchema = z.string().regex(
-            /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/,
-            { message: "Invalid IP address format" }
-        );
+        const ipSchema = z
+            .string()
+            .regex(
+                /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/,
+                { message: "Invalid IP address format" }
+            );
 
         // Definizione dello schema per la porta
-        const portSchema = z.string().regex(
-            /^([0-9]{1,5})$/,
-            { message: "Invalid port format" }
-        ).refine((port) => {
-            const numPort = Number(port);
-            return numPort >= 1 && numPort <= 65535;
-        }, { message: "Port must be between 1 and 65535" });
+        const portSchema = z
+            .string()
+            .regex(/^([0-9]{1,5})$/, { message: "Invalid port format" })
+            .refine(
+                (port) => {
+                    const numPort = Number(port);
+                    return numPort >= 1 && numPort <= 65535;
+                },
+                { message: "Port must be between 1 and 65535" }
+            );
 
         // Definizione dello schema per username e password
         const userInfoSchema = z.string().nonempty();
@@ -56,27 +61,38 @@ class Proxy implements ProxyI {
 
         // Funzione di validazione della stringa proxy
         const proxySchema = z.union([
-            z.string().refine((proxy) => {
-                const parts = proxy.split(':');
-                if (parts.length === 2) {
-                    const [ip, port] = parts;
-                    return ipPortSchema.safeParse({ ip, port }).success;
-                }
-                return false;
-            }, { message: "Invalid ip:port format" }),
-            z.string().refine((proxy) => {
-                const parts = proxy.split(':');
-                if (parts.length === 4) {
-                    const [ip, port, username, password] = parts;
-                    return ipPortAuthSchema.safeParse({ ip, port, username, password }).success;
-                }
-                return false;
-            }, { message: "Invalid ip:port:username:password format" }),
+            z.string().refine(
+                (proxy) => {
+                    const parts = proxy.split(":");
+                    if (parts.length === 2) {
+                        const [ip, port] = parts;
+                        return ipPortSchema.safeParse({ ip, port }).success;
+                    }
+                    return false;
+                },
+                { message: "Invalid ip:port format" }
+            ),
+            z.string().refine(
+                (proxy) => {
+                    const parts = proxy.split(":");
+                    if (parts.length === 4) {
+                        const [ip, port, username, password] = parts;
+                        return ipPortAuthSchema.safeParse({
+                            ip,
+                            port,
+                            username,
+                            password,
+                        }).success;
+                    }
+                    return false;
+                },
+                { message: "Invalid ip:port:username:password format" }
+            ),
         ]);
 
         let parsedProxyString: string;
         try {
-            parsedProxyString = proxySchema.parse(stringProxy)
+            parsedProxyString = proxySchema.parse(stringProxy);
 
             const parts = parsedProxyString.split(":");
             if (parts.length === 4) {
@@ -85,44 +101,71 @@ class Proxy implements ProxyI {
                 this.port = port;
                 this.auth = {
                     username: username,
-                    password: password
-                }
+                    password: password,
+                };
             } else {
                 const [ip, port] = parts;
                 this.ip = ip;
                 this.port = port;
             }
         } catch (err) {
-            throw new Error(`${stringProxy} is invalid: ${(err as z.ZodError).errors.map(err => err.message).join(', ')}`)
+            throw new Error(
+                `${stringProxy} is invalid: ${(err as z.ZodError).errors
+                    .map((err) => err.message)
+                    .join(", ")}`
+            );
         }
     }
 
     fullProxy(): string {
-        return `${this.ip}:${this.port}${this.auth ? `${this.auth.username}:${this.auth.password}` : ""}`
+        return `${this.ip}:${this.port}${
+            this.auth ? `${this.auth.username}:${this.auth.password}` : ""
+        }`;
     }
 }
 
 export class ProxyChooser {
     private proxyes: string[] = [];
     readonly proxyFilePath: string;
+    readonly cachePath: string;
     readonly proxyCache = new Collection<string, Proxy>();
 
-    constructor(proxyFilePath: string = path.join(__dirname, "..", "..", "data", "proxyes.txt")) {
+    constructor(
+        proxyFilePath: string = path.join(
+            __dirname,
+            "..",
+            "..",
+            "data",
+            "proxyes.txt"
+        ),
+        cachePath: string = path.join(
+            __dirname,
+            "..",
+            "..",
+            "data",
+            "proxyCache.json"
+        )
+    ) {
         this.proxyFilePath = proxyFilePath;
+        this.cachePath = cachePath;
         this.loadFile();
     }
 
     loadFile(): void {
         let proxyStringList: string;
         try {
-            proxyStringList = fs.readFileSync(this.proxyFilePath, { encoding: "utf8" });
+            proxyStringList = fs.readFileSync(this.proxyFilePath, {
+                encoding: "utf8",
+            });
         } catch (err) {
-            console.log("❌ There was an error in ProxyChoser.loadfile")
+            console.log("❌ There was an error in ProxyChoser.loadfile");
             throw err;
         }
 
         let proxyArray: string[] = proxyStringList.split("\n");
-        proxyArray = proxyArray.map((proxy) => proxy.trim().replace("\r", "").replace("\n", ""));
+        proxyArray = proxyArray.map((proxy) =>
+            proxy.trim().replace("\r", "").replace("\n", "")
+        );
         proxyArray = proxyArray.filter((proxy) => proxy.length > 0);
 
         this.proxyes = proxyArray;
@@ -130,7 +173,7 @@ export class ProxyChooser {
 
     // Wrapper for the 2 function below, it automaticaly get the proxy and set it in the cache, and mange errors
     /**
-     * 
+     *
      * @param token
      * The token that will be associate with the proxy. The token and the proxy will be cached and every time that a proxy is request with the same token the proxy associate with that token will be retuned from cache
      */
@@ -145,11 +188,14 @@ export class ProxyChooser {
         // We set a value for this variabile or typescript will give an error saying that the variabile is used before assegnation
         let randomProxy: Proxy = new Proxy("0.0.0.0:1111:a:b");
         while (existInCache) {
-            const randomStringProxy = this.proxyes[Math.floor(Math.random() * this.proxyes.length)];
-            console.log(randomStringProxy)
+            const randomStringProxy =
+                this.proxyes[Math.floor(Math.random() * this.proxyes.length)];
+            console.log(randomStringProxy);
             // const randomProxy = this.fromStringProxyToProxyObject(randomStringProxy)
             randomProxy = new Proxy(randomStringProxy);
-            existInCache = this.proxyCache.some((proxy) => proxy === randomProxy)
+            existInCache = this.proxyCache.some(
+                (proxy) => proxy === randomProxy
+            );
         }
 
         this.setProxyInCache(token, randomProxy);
@@ -160,7 +206,7 @@ export class ProxyChooser {
     private getProxyFromCache(token: string): Proxy | undefined {
         if (token.length == 0) throw new Error("insert a valid token");
 
-        const proxy = this.proxyCache.get(token)
+        const proxy = this.proxyCache.get(token);
         return proxy;
     }
 
@@ -170,17 +216,15 @@ export class ProxyChooser {
         this.proxyCache.set(token, proxy);
     }
 
-    // loadCache(): void {
+    loadCache(): void {}
 
-    // }
+    saveCahche(): void {}
 
-    // saveCahche(): void {
+    clearCache(): void {}
 
-    // }
-
-    // clearCache(): void {
-
-    // }
+    private cacheFileController(action: "read" | "write"): void {
+        const data = fs.openSync(this.cachePath);
+    }
 }
 
 // Testing
